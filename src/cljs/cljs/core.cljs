d1b6310202690ea9c03fba5c4402c25c6fbd44dc
fogus <mefogus@gmail.com>
2011-06-20 07:39:05 -0400

    Added tree-seq

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -354,6 +354,21 @@
           (cons f (filter pred r))
           (filter pred r)))))))
 
+(defn tree-seq
+  "Returns a lazy sequence of the nodes in a tree, via a depth-first walk.
+   branch? must be a fn of one arg that returns true if passed a node
+   that can have children (but may not).  children must be a fn of one
+   arg that returns a sequence of the children. Will only be called on
+   nodes for which branch? returns true. Root is the root node of the
+  tree."
+   [branch? children root]
+   (let [walk (fn walk [node]
+                (lazy-seq
+                 (cons node
+                  (when (branch? node)
+                    (mapcat walk (children node))))))]
+     (walk root)))
+
 ; simple reduce, to be removed when IReduce is working
 (defn reduce
   ([f coll]
