7698b6e45941674a03461766e5f8ff57b6ef55b9
Christopher Redinger <redinger@gmail.com>
2011-07-05 00:39:41 -0400

    Removing incorrect documentation about assoc on vectors

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -377,7 +377,7 @@ reduces them without incurring seq initialization"
   "assoc[iate]. When applied to a map, returns a new map of the
    same (hashed/sorted) type, that contains the mapping of key(s) to
    val(s). When applied to a vector, returns a new vector that
-   contains val at index. Note - index must be <= (count vector)."
+   contains val at index."
   [coll k v]
   (-assoc coll k v))
 
