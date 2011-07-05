6d686591a3d8b014ef5918bc6dd1d14ca55f2eb2
Christopher Redinger <redinger@gmail.com>
2011-07-04 23:34:24 -0400

    Removing reference to Java arrays and Iterable from seq documentation.

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -285,8 +285,7 @@ reduces them without incurring seq initialization"
 (defn seq
   "Returns a seq on the collection. If the collection is
   empty, returns nil.  (seq nil) returns nil. seq also works on
-  Strings, native Java arrays (of reference types) and any objects
-  that implement Iterable."
+  Strings."
   [coll]
   (when coll
     (-seq coll)))
