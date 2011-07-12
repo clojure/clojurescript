82c576266e4594ae02be17f34e764ada4c001c11
Brenton Ashworth <bashwort@gmail.com>
2011-07-12 09:59:02 -0400

    Build scripts use cljs.closure/build

--- bin/cljsc.clj
+++ bin/cljsc.clj
@@ -6,6 +6,16 @@
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 
-(require '[cljs.project-compiler :as comp])
+(require '[cljs.closure :as closure])
 
-(apply comp/compile-project *command-line-args*)
+(defn transform-cl-args
+  [args]
+  (let [source (first args)
+        opts-string (apply str (interpose " " (rest args)))
+        options (when (> (count opts-string) 1)
+                  (try (read-string opts-string)
+                       (catch Exception e (println e))))]
+    {:source source :options (merge {:output-to :print} options)}))
+
+(let [args (transform-cl-args *command-line-args*)]
+  (closure/build (:source args) (:options args)))
