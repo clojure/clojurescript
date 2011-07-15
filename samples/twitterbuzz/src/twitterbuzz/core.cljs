7f463ace1ef2c409486f881ac8d84f2dc61064d7
Alan Dipert and Brenton Ashworth <pair@thinkrelevance.com>
2011-07-15 16:00:49 -0400

    (WIP) Jsonp progress, refs #48

--- samples/twitterbuzz/src/twitterbuzz/core.cljs
+++ samples/twitterbuzz/src/twitterbuzz/core.cljs
@@ -1,1 +1,18 @@
-(ns twitterbuzz.core)
+(ns twitterbuzz.core
+  (:require [goog.net.Jsonp :as jsonp]
+            [goog.debug.Console :as console]))
+
+(def console (goog.debug.Console.))
+(defn log [msg] (.addLogRecord console msg))
+
+(def twitter-uri (goog.Uri. "http://twitter.com/search.json"))
+
+(defn retrieve [payload callback]
+  (.send (goog.net.Jsonp. twitter-uri)
+         payload
+         callback))
+
+(defn my-callback [json]
+  (log json))
+
+(retrieve (.strobj {"q" "clojure"}) my-callback)
