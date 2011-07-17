4ae740f4bde83c9ecc9773fa15bfd505afa183a9
Brenton Ashworth <bashwort@gmail.com>
2011-07-17 01:22:19 -0400

    (WIP) Improve demo app. We now have tweets on the page. Refs #48

--- samples/twitterbuzz/src/twitterbuzz/core.cljs
+++ samples/twitterbuzz/src/twitterbuzz/core.cljs
@@ -1,21 +1,18 @@
 (ns twitterbuzz.core
   (:require [goog.net.Jsonp :as jsonp]
-            [goog.debug :as debug]
-            [goog.debug.FancyWindow :as fancy]
-            [goog.debug.Logger :as logger]))
+            [goog.Timer :as timer]
+            [goog.events :as events]
+            [goog.dom :as dom]))
 
-;; Temporary way to see results
-(def debug-window
-  (let [w (goog.debug.FancyWindow. "Twitterbuzz")
-        _ (.setEnabled w true)
-        _ (.init w)]
-    w))
+;; From Chouser
+(defn js-obj-seq [o]
+  (for [k (js-keys o)] [(keyword k) (aget o k)]))
 
-(def log (logger/getLogger "Twitterbuzz Logger"))
-
-(defn show [x]
-  (.info log (debug/expose x)))
-;; remove this logging code once we can see results on the page
+(defn obj->map
+  "Convert a JavaScript object into a Clojure map. Can we have
+  something like this in core?"
+  [o]
+  (into {} (js-obj-seq o)))
 
 (def twitter-uri (goog.Uri. "http://twitter.com/search.json"))
 
@@ -24,42 +21,86 @@
          payload
          callback))
 
-;; From Chouser - We need something like this in core.
-(defn js-obj-seq [o]
-  (for [k (js-keys o)] [(keyword k) (aget o k)]))
-
-(defn obj->map
-  "Convert a JavaScript object into a Clojure map."
-  [o]
-  (into {} (js-obj-seq o)))
-
 (def state (atom {:max-id 1 :functions []}))
+
+(defn send-tweets [fns tweets]
+  (when (seq fns)
+    (do ((first fns) tweets)
+        (recur (rest fns) tweets))))
 
 (defn my-callback [json]
   (let [result-map (obj->map json)
-        max-id (:max_id result-map)
-        tweets (filter #(> (:id %) (:max-id @state))
+        new-max (:max_id result-map)
+        old-max (:max-id @state) ;; the filter won't work if you inline this
+        tweets (filter #(> (:id %) old-max)
                        (map obj->map (:results result-map)))]
-    ;; tweets should always be the newest tweets.
-    (do (show (str (count tweets)))
-        (swap! state (fn [old] (assoc old :max-id max-id)))
-        (show (str (:max-id @state)))
-        ;; Call registered functions here
-        )))
-
-(retrieve (.strobj {"q" "clojure"}) my-callback)
+    (do  (swap! state (fn [old] (assoc old :max-id new-max)))
+         (send-tweets (:functions @state) tweets))))
 
 (defn register
   "Register fn to be called with new tweets."
-  [fn])
+  [f]
+  (swap! state (fn [old] (assoc old :functions (conj (:functions old) f)))))
+
+(defn search-tag
+  "Get the current tag value from the page."
+  []
+  (.value (dom/getElement "twitter-search-tag")))
+
+(defn listener []
+  (retrieve (.strobj {"q" (search-tag)}) my-callback))
 
 (defn poll
+  "Request new data from twitter once every 24 seconds. This will put
+  you at the 150 request/hour rate limit. We can speed it up for the demo."
   []
-  ;; polls twitter periodically using jsonp
-  ;; figures out if there are any new tweets
-  ;;   drop the n we already have?
-  ;;   use the latest known timestamp?
-  ;; if there are new tweets
-  ;;   keywordize the JSON keys
-  ;;   call every registered fn with the vector of maps of new tweets.
-  )
+  (let [timer (goog.Timer. 24000)]
+    (do (listener)
+        (.start timer nil) ;; doesn't work as (.start timer)
+        (events/listen timer goog.Timer/TICK listener))))
+
+;; Rendering Stuff - Replace with something cool
+;; =============================================
+
+(defn dom-element [element attrs]
+  (dom/createDom element
+                 (.strobj (reduce (fn [m [k v]]
+                                    (assoc m k v))
+                                  {}
+                                  (map #(vector (name %1) %2) (keys attrs) (vals attrs))))))
+
+(defn html [s]
+  (dom/htmlToDocumentFragment s))
+
+(defn add-leaderboard-tweet [tweet]
+  (let [parent (dom/getElement "leaderboard")
+        child (dom-element "div" {:class "tweet"})
+        user (dom-element "div" {:class "user-name"})
+        text (dom-element "div" {:class "tweet-text"})
+        pic (dom-element "img" {:src (:profile_image_url tweet) :class "profile-pic"})]
+    (do (dom/setTextContent text (:text tweet))
+        (dom/setTextContent user (:from_user tweet))
+        (dom/appendChild child pic)
+        (dom/appendChild child user)
+        (dom/appendChild child text)
+        (dom/appendChild parent child))))
+
+(defn clear-leaderboard []
+  (let [parent (dom/getElement "leaderboard")
+        title (dom/getFirstElementChild parent)]
+    (do (dom/removeChildren parent)
+        (dom/appendChild parent title))))
+
+;; Example function - waiting for tweets.
+(defn update-view-fn [tweets]
+  (let [status (dom/getElement "tweet-status")]
+    (do (clear-leaderboard)
+        (dom/setTextContent status (str (count tweets) " tweets"))
+        (loop [tweets tweets]
+          (when (seq tweets)
+            (do (add-leaderboard-tweet (first tweets))
+                (recur (rest tweets))))))))
+
+(register update-view-fn)
+
+(poll)
