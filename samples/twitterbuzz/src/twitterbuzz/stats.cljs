436870c5dc1faed7dfc23ef19f5fd6270e1887b8
Alex Redington <alex.redington@thinkrelevance.com>
2011-07-18 14:04:07 -0400

    graph calculation bits, WIP

--- samples/twitterbuzz/src/twitterbuzz/stats.cljs
+++ samples/twitterbuzz/src/twitterbuzz/stats.cljs
@@ -19,8 +19,44 @@
         tweet-ids (keys retweet-counts)]
     (take n (sort-by #(get retweet-counts %) tweet-ids))))
 
+(defn retweet-of
+  "Returns the username of the user who originally authored the tweet
+  that t is a retweet of, nil otherwise"
+  [t]
+  (get-in t ["retweeted_status" "user" "name"]))
+
+(defn mentioned-users
+  "Returns the usernames of all the users mentioned in tweet t"
+  [t]
+  (map #(get % "screen_name") (get-in t ["entities" "user_mentions"])))
+
+(defn new-edges
+  "Filter the seq of sets n to find all sets in n that do not already
+  exist in the seq of sets o"
+  [n o]
+  (filter #(not (contains? o %)) n))
+
+(defn edges
+  "Return a lazy seq of pairs of usernames representing the social edges of a tweet
+  stream, where a social edge is defined as one user retweeting or
+  mentioning another user in a given tweet. Edges are not directional; if
+  @alice retweets @bob, and @bob mentions @alice, those two tweets only create
+  one edge."
+  ([ts]
+     (edges ts #{}))
+  ([ts edges]
+     (let [t (first ts)
+           other-users (concat (list (retweet-of t)) (mentioned-users t))
+           author (get-in t ["user" "name"])
+           tweet-edges (set (map #(set (list author %)) other-users))
+           new-edges (new-edges tweet-edges edges)]
+       (when t
+         (if (empty? new-edges)
+           (recur (rest ts) edges)
+           (lazy-seq (concat new-edges (edges (rest ts) (concat new-edges edges)))))))))
+
 (defn graph
   "Returns a map representing the graph of the tweet seq ts"
   [ts]
-  {:nodes []
+  {:nodes {}
    :edges []})
