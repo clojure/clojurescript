12474fdd4e5b2c9076b2a0434ff0733bfa2f0a31
Alex Redington <alex.redington@thinkrelevance.com>
2011-07-18 14:16:16 -0400

    Implement the improvements from Stu's feedback.

--- samples/twitterbuzz/src/twitterbuzz/stats.cljs
+++ samples/twitterbuzz/src/twitterbuzz/stats.cljs
@@ -1,45 +1,40 @@
-(ns twitterbuzz.graph)
+(ns twitterbuzz.stats)
 
 ;; code review from SDH:
 ;;
-;; 1. I think inc-or-nil could be replaced by fnil
+;; 1. I think inc-or-nil could be replaced by fnil - fixed ar
 ;; 2. update-retweet-count looks broken
-;;    `when` instead of `if` in function to be reduced over
+;;    `when` instead of `if` in function to be reduced over - fixed ar
 ;; 3. conversion from string keys to keywords should be
-;;    done already before we get here
+;;    done already before we get here - fixed ar 
 ;; 4. fns like top-tweets should not take an n arg
-;;    consumers can always take what they want
-;; 5. namespace and file name do not match
-
-(defn inc-or-1
-  "Returns 1 if x is nil, (inc x) otherwise"
-  [x]
-  (if (nil? x) 1 (inc x)))
+;;    consumers can always take what they want - fixed ar
+;; 5. namespace and file name do not match - fixed ar
 
 (defn update-retweet-count
   "Updates the total count of retweets by id in m as indicated by the contents of
   the tweet t"
   [m t]
-  (let [rt-id (get-in t ["retweeted_status" "id_str"])]
-    (when-not (nil? rt-id) (update-in m [rt-id] inc-or-1))))
+  (let [rt-id (get-in t [:retweeted_status :id_str])]
+    (if (nil? rt-id) m (update-in m [rt-id] (fnil inc 0)))))
 
 (defn top-tweets
   "Returns the ids of top n most retweeted tweets out of tweet seq ts"
-  [ts n]
+  [ts]
   (let [retweet-counts (reduce update-retweet-count {} ts)
         tweet-ids (keys retweet-counts)]
-    (take n (sort-by #(get retweet-counts %) tweet-ids))))
+    (sort-by #(get retweet-counts %) tweet-ids)))
 
 (defn retweet-of
   "Returns the username of the user who originally authored the tweet
   that t is a retweet of, nil otherwise"
   [t]
-  (get-in t ["retweeted_status" "user" "name"]))
+  (get-in t [:retweeted_status :user :name]))
 
 (defn mentioned-users
   "Returns the usernames of all the users mentioned in tweet t"
   [t]
-  (map #(get % "screen_name") (get-in t ["entities" "user_mentions"])))
+  (map #(get % :screen_name) (get-in t [:entities :user_mentions])))
 
 (defn new-edges
   "Filter the seq of sets n to find all sets in n that do not already
@@ -58,7 +53,7 @@
   ([ts edges]
      (let [t (first ts)
            other-users (concat (list (retweet-of t)) (mentioned-users t))
-           author (get-in t ["user" "name"])
+           author (get-in t [:user :name])
            tweet-edges (set (map #(set (list author %)) other-users))
            new-edges (new-edges tweet-edges edges)]
        (when t
