cf84fa2cc1ce0e6f13249baef6cd71a635568bcc
David Nolen <david.nolen@gmail.com>
2011-10-14 09:31:44 -0400

    * src/clj/cljs/core.clj: CLJS-88: bug in javascript generated by condp

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -34,7 +34,7 @@
                  :imported)))
 
 (import-macros clojure.core
- [-> ->> ..  and assert comment cond condp
+ [-> ->> ..  and assert comment cond
   declare defn defn-
   doto
   extend-protocol fn for
@@ -396,6 +396,48 @@
          ~@(map
             (fn [[k v]] (list 'set! k v))
             resets))))))
+
+(defmacro condp
+  "Takes a binary predicate, an expression, and a set of clauses.
+  Each clause can take the form of either:
+
+  test-expr result-expr
+
+  test-expr :>> result-fn
+
+  Note :>> is an ordinary keyword.
+
+  For each clause, (pred test-expr expr) is evaluated. If it returns
+  logical true, the clause is a match. If a binary clause matches, the
+  result-expr is returned, if a ternary clause matches, its result-fn,
+  which must be a unary function, is called with the result of the
+  predicate as its argument, the result of that call being the return
+  value of condp. A single default expression can follow the clauses,
+  and its value will be returned if no clause matches. If no default
+  expression is provided and no clause matches, an
+  IllegalArgumentException is thrown."
+  {:added "1.0"}
+
+  [pred expr & clauses]
+  (let [gpred (gensym "pred__")
+        gexpr (gensym "expr__")
+        emit (fn emit [pred expr args]
+               (let [[[a b c :as clause] more]
+                       (split-at (if (= :>> (second args)) 3 2) args)
+                       n (count clause)]
+                 (cond
+                  (= 0 n) `(throw (js/Error. (str "No matching clause: " ~expr)))
+                  (= 1 n) a
+                  (= 2 n) `(if (~pred ~a ~expr)
+                             ~b
+                             ~(emit pred expr more))
+                  :else `(if-let [p# (~pred ~a ~expr)]
+                           (~c p#)
+                           ~(emit pred expr more)))))
+        gres (gensym "res__")]
+    `(let [~gpred ~pred
+           ~gexpr ~expr]
+       ~(emit gpred gexpr clauses))))
 
 (defmacro try
   "(try expr* catch-clause* finally-clause?)
