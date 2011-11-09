91d0438a6037c35377c42705b3eb87288d92f704
fogus <mefogus@gmail.com>
2011-11-09 09:11:20 -0500

    Generalized the dot form classification

--- src/clj/cljs/compiler.clj
+++ src/clj/cljs/compiler.clj
@@ -852,31 +852,28 @@
      (-> sym name (.substring 1) munge)
      (-> sym name munge))))
 
-(defn- classify-sub
-  [e]
-  (cond (property-symbol? e) ::property
-        (symbol? e)          ::symbol
-        (seq? e)             ::list
-        (nil? e)             ()
-        :default             ::unknown))
+(defn- classify-dot-form
+  [[target member args]]
+  [(cond (nil? target) ::error
+         :default      ::expr)
+   (cond (property-symbol? member) ::property
+         (symbol? member)          ::symbol    
+         (seq? member)             ::list
+         :default                  ::error)
+   (cond (nil? args) ()
+         :default    ::expr)])
 
-(defmulti build-dot-form #(map classify-sub %))
+(defmulti build-dot-form #(classify-dot-form %))
 
-;; (.-p (...))
-;; (. (...) -p)
-(defmethod build-dot-form [::list ::property ()]
-  [[target prop _]]
-  {:dot-action ::access :target target :field (clean-symbol prop)})
 
-;; (.-p o)
 ;; (. o -p)
-(defmethod build-dot-form [::symbol ::property ()]
+;; (. (...) -p)
+(defmethod build-dot-form [::expr ::property ()]
   [[target prop _]]
   {:dot-action ::access :target target :field (clean-symbol prop)})
 
-;; (.-p o <args>)
 ;; (. o -p <args>)
-(defmethod build-dot-form [::symbol ::property ::list]
+(defmethod build-dot-form [::expr ::property ::list]
   [[target prop args]]
   (throw (Error. (str "Cannot provide arguments " args " on property access " prop))))
 
@@ -888,37 +885,27 @@
     {:dot-action ::call :target target :method (munge meth) :args args}
     {:dot-action ::call :target target :method (munge (first meth)) :args args}))
 
-;; (.m o 1 2)
 ;; (. o m 1 2)
-(defmethod build-dot-form [::symbol ::symbol ::list]
+(defmethod build-dot-form [::expr ::symbol ::expr]
   [[target meth args]]
   (build-method-call target meth args))
 
-;; (.m o)
 ;; (. o m)
-(defmethod build-dot-form [::symbol ::symbol ()]
+(defmethod build-dot-form [::expr ::symbol ()]
   [[target meth args]]
   (build-method-call target meth args))
 
 ;; (. o (m))
 ;; (. o (m 1 2))
-(defmethod build-dot-form [::symbol ::list ()]
-  [[target [meth & args] _]]
-  (build-method-call target meth args))
-
 ;; (. (. ...))
-(defmethod build-dot-form [::list ::list ()]
+;; (. (f _) m 1 2)
+(defmethod build-dot-form [::expr ::list ()]
   [[target meth _]]
   (build-method-call target meth ()))
 
-;; (.m (f _) 1 2)
-(defmethod build-dot-form [::list ::symbol ::list]
-  [[target meth args]]
-  (build-method-call target meth args))
-
 (defmethod build-dot-form :default
   [dot-form]
-  (throw (Error. (str "Unknown dot form of " dot-form " with cassification " (vec (map classify-sub dot-form))))))
+  (throw (Error. (str "Unknown dot form of " (list* '. dot-form) " with classification " (classify-dot-form dot-form)))))
 
 (defmethod parse '.
   [_ env [_ target & [field & member+]] _]
