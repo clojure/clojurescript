bb2e10f20beaa7b5d3afe01c3544384d1c40ef1c
David Nolen <david.nolen@gmail.com>
2011-11-20 14:47:03 -0500

    * src/clj/cljs/core.clj: CLJS-97: fix record equality. made identical? inlineable.

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -15,7 +15,7 @@
                             memfn ns or proxy proxy-super pvalues refer-clojure reify sync time
                             when when-first when-let when-not while with-bindings with-in-str
                             with-loading-context with-local-vars with-open with-out-str with-precision with-redefs
-                            satisfies?
+                            satisfies? identical?
 
                             aget aset
                             + - * / < <= > >= == zero? pos? neg? inc dec max min mod
@@ -41,6 +41,9 @@
   if-let if-not let letfn loop
   or
   when when-first when-let when-not while])
+
+(defmacro identical? [a b]
+  (list 'js* "(~{} === ~{})" a b))
 
 (defmacro aget [a i]
   (list 'js* "(~{}[~{}])" a i))
@@ -278,7 +281,10 @@
 		  'IHash
 		  `(~'-hash [this#] (hash-coll this#))
 		  'IEquiv
-		  `(~'-equiv [this# other#] (equiv-map this# other#))
+		  `(~'-equiv [this# other#]
+         (and (identical? (.constructor this#) ;; TODO: change for prop lookup
+                          (.constructor other#))
+              (equiv-map this# other#)))
 		  'IMeta
 		  `(~'-meta [this#] ~'__meta)
 		  'IWithMeta
