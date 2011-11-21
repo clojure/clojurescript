b9e8ac8ddb3c8d4e42f11a5ab05a3627cc24c73a
David Nolen <david.nolen@gmail.com>
2011-11-21 00:29:52 -0500

    * src/clj/cljs/core.clj: remove hasOwnProperty test - this code won't work with advanced compilation. Sadly in JS we can't differentiate between a prototype and an actual instance of a type.

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -386,9 +386,9 @@
   (let [p (:name (cljs.compiler/resolve-var (dissoc &env :locals) psym))
         prefix (protocol-prefix p)]
     `(let [x# ~x]
-       (if (and x# (. x# ~(symbol prefix)) (not (. x# (~'hasOwnProperty ~prefix))))
-	 true
-	 (cljs.core/type_satisfies_ ~psym x#)))))
+       (if (and x# (. x# ~(symbol prefix)))
+         true
+         (cljs.core/type_satisfies_ ~psym x#)))))
 
 (defmacro lazy-seq [& body]
   `(new cljs.core.LazySeq nil false (fn [] ~@body)))
