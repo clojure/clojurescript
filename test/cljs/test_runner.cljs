43fd784303edd3c1f4818387a4fc7d98c9988911
Kevin J. Lynagh <kevin@keminglabs.com>
2012-01-04 11:02:14 -0800

    Add test demonstrating that (:refer-clojure :exclude []) doesn't exclude functions defined in cljs.core.

--- test/cljs/test_runner.cljs
+++ test/cljs/test_runner.cljs
@@ -1,6 +1,7 @@
 (ns test-runner
   (:require [cljs.core-test :as core-test]
             [cljs.reader-test :as reader-test]
+            [cljs.ns-test :as ns-test]
             [clojure.string-test :as string-test]))
 
 (set! *print-fn* js/print)
@@ -8,6 +9,7 @@
 (core-test/test-stuff)
 (reader-test/test-reader)
 (string-test/test-string)
+(ns-test/test-ns)
 
 (println "Tests completed without exception")
 
