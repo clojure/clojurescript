0df1bc4d7bdf4d411e72d443b5a23d146d5e64b8
David Nolen <david.nolen@gmail.com>
2012-01-11 00:46:03 -0500

    * test/cljs/cljs/binding_test.cljs: test for binding referred vars fix

--- test/cljs/test_runner.cljs
+++ test/cljs/test_runner.cljs
@@ -1,6 +1,7 @@
 (ns test-runner
   (:require [cljs.core-test :as core-test]
             [cljs.reader-test :as reader-test]
+            [cljs.binding-test :as binding-test]
             [cljs.ns-test :as ns-test]
             [clojure.string-test :as string-test]))
 
@@ -9,6 +10,7 @@
 (core-test/test-stuff)
 (reader-test/test-reader)
 (string-test/test-string)
+(binding-test/test-binding)
 (ns-test/test-ns)
 
 (println "Tests completed without exception")
