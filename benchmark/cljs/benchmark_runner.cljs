62d67bb09a1a9f0be22831ac47c98defdfddf85c
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-05-18 01:49:04 -0400

    * benchmark/cljs/benchmark_runner.cljs: add reduce + benchmark

--- benchmark/cljs/benchmark_runner.cljs
+++ benchmark/cljs/benchmark_runner.cljs
@@ -25,4 +25,8 @@
 (simple-benchmark [coll [1 2 3]] (next coll) 1000000)
 (println)
 
+(println ";;; seq ops")
+(simple-benchmark [coll (range 500000)] (reduce + coll) 1)
+(println)
+
 (println "\n")
