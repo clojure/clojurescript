1500a113efcffb1af9169e689dda4e0f20a0756c
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-05-19 01:21:39 -0400

    * benchmark/cljs/benchmark_runner.cljs: add map related benchmarks

--- benchmark/cljs/benchmark_runner.cljs
+++ benchmark/cljs/benchmark_runner.cljs
@@ -28,6 +28,12 @@
 (simple-benchmark [coll [1 2 3]] (next coll) 1000000)
 (println)
 
+(println ";;; map ops")
+(simple-benchmark [coll {:foo 1 :bar 2}] (get coll :foo) 1000000)
+(simple-benchmark [coll {:foo 1 :bar 2}] (-lookup coll :foo nil) 1000000)
+(simple-benchmark [coll {:foo 1 :bar 2}] (assoc coll :baz 3) 100000)
+(println)
+
 (println ";;; seq ops")
 (simple-benchmark [coll (range 500000)] (reduce + coll) 1)
 (println)
