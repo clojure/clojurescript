431842358b21412fdf3cb4540bc5b93503d409f4
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-05-28 19:16:07 -0400

    * src/clj/cljs/compiler.clj: optimize higher-order invocation of cljs fns

--- src/clj/cljs/compiler.clj
+++ src/clj/cljs/compiler.clj
@@ -751,7 +751,10 @@
        (emits f "(" (comma-sep args)  ")")
        
        :else
-       (emits f ".call(" (comma-sep (cons "null" args)) ")")))))
+       (if (= (:op f) :var)
+         (let [fprop (str ".cljs$lang$arity$" (count args))]
+           (emits "(" f fprop " ? " f fprop "(" (comma-sep args) ") : " f ".call(" (comma-sep (cons "null" args)) "))"))
+         (emits f ".call(" (comma-sep (cons "null" args)) ")"))))))
 
 (defmethod emit :new
   [{:keys [ctor args env]}]
