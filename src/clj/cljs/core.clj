2aeb3d8f6fc4fc65112b62bc89de41eae7d0fe1f
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-06-08 15:50:28 -0400

    * src/clj/cljs/core.clj: fix fast path protocols

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -45,7 +45,7 @@
 
 (def fast-path-protocols
   "protocol fqn -> [partition number, bit]"
-  (zipmap (map #(symbol (core/str "cljs.core." %))
+  (zipmap (map #(symbol "cljs.core" (core/str %))
                '[IFn ICounted IEmptyableCollection ICollection IIndexed ASeq ISeq INext
                  ILookup IAssociative IMap IMapEntry ISet IStack IVector IDeref
                  IDerefWithTimeout IMeta IWithMeta IReduce IKVReduce IEquiv IHash
