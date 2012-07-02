56adcc5d13191cc29d48acbe020d9fc0500ce853
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-07-02 13:11:09 -0400

    * src/cljs/cljs/core.cljs: fix typo

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -2654,7 +2654,7 @@ reduces them without incurring seq initialization"
 
 (defn get-in
   "Returns the value in a nested associative structure,
-  where ks is a sequence of ke(ys. Returns nil if the key is not present,
+  where ks is a sequence of keys. Returns nil if the key is not present,
   or the not-found value if supplied."
   {:added "1.2"
    :static true}
