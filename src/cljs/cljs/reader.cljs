df6f316acdc3375ea6de29b7700dac1a5c8e9dbe
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-08-17 01:40:21 -0400

    CLJS-356: make cljs.reader errors conform to Clojure JVM.

--- src/cljs/cljs/reader.cljs
+++ src/cljs/cljs/reader.cljs
@@ -234,7 +234,7 @@ nil if the end of stream has been reached")
   [delim rdr recursive?]
   (loop [a (transient [])]
     (let [ch (read-past whitespace? rdr)]
-      (when-not ch (reader-error rdr "EOF"))
+      (when-not ch (reader-error rdr "EOF while reading"))
       (if (identical? delim ch)
         (persistent! a)
         (if-let [macrofn (macros ch)]
@@ -303,7 +303,7 @@ nil if the end of stream has been reached")
   (loop [buffer (gstring/StringBuffer.)
          ch (read-char reader)]
     (cond
-     (nil? ch) (reader-error reader "EOF while reading string")
+     (nil? ch) (reader-error reader "EOF while reading")
      (identical? "\\" ch) (recur (do (.append buffer (escape-char buffer reader)) buffer)
                         (read-char reader))
      (identical? \" ch) (. buffer (toString))
@@ -418,7 +418,7 @@ nil if the end of stream has been reached")
   [reader eof-is-error sentinel is-recursive]
   (let [ch (read-char reader)]
     (cond
-     (nil? ch) (if eof-is-error (reader-error reader "EOF") sentinel)
+     (nil? ch) (if eof-is-error (reader-error reader "EOF while reading") sentinel)
      (whitespace? ch) (recur reader eof-is-error sentinel is-recursive)
      (comment-prefix? ch) (recur (read-comment reader ch) eof-is-error sentinel is-recursive)
      :else (let [f (macros ch)
