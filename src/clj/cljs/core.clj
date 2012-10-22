eab6032e6ba22571e5c4f821707bf5de8075e757
Paul deGrandis <paul.degrandis@gmail.com>
2012-10-22 14:54:24 -0700

    Added support for with-out-str using *print-fn* [cljs-321]

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -1156,3 +1156,14 @@
            (~'f)
            ~(gen-apply-to-helper))))
      (set! ~'*unchecked-if* false)))
+
+(defmacro with-out-str
+  "Evaluates exprs in a context in which *print-fn* is bound to .append
+  on a fresh StringBuffer.  Returns the string created by any nested
+  printing calls."
+  [& body]
+  `(let [sb# (goog.string/StringBuffer.)]
+     (binding [cljs.core/*print-fn* (fn [x#] (.append sb# x#))]
+       ~@body)
+     (cljs.core/str sb#)))
+
