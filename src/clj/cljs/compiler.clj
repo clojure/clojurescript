269a130e3f02d1f71312863f4f4e7c3f9457cdc9
magomimmo <mimmo.cosenza@gmail.com>
2012-12-02 04:27:55 +0100

    correct exclude-file-names

--- src/clj/cljs/compiler.clj
+++ src/clj/cljs/compiler.clj
@@ -881,13 +881,23 @@
                      (not (contains? excluded-set path))))
              (file-seq dir))))
 
+;; (defn exclude-file-names
+;;   "Return a set of absolute paths of files that must be excluded"
+;;   [dir exclude-coll]
+;;   (set (filter #(.endsWith ^String % ".cljs")
+;;           (map #(.getAbsolutePath ^java.io.File %)
+;;                (mapcat #(let [dir-path (.getAbsolutePath ^java.io.File dir)]
+;;                           (file-seq (io/file (str dir-path java.io.File/separator %))))
+;;                        exclude-coll)))))
+
 (defn exclude-file-names
   "Return a set of absolute paths of files to be excluded"
   [dir exclude-vec]
-  (set (filter #(.endsWith ^String % ".cljs")
-               (map #(.getAbsolutePath ^java.io.File %)
-                    (mapcat #(file-seq (io/file (str dir java.io.File/separator %)))
-                            exclude-vec)))))
+  (when (and dir exclude-vec)
+    (set (filter #(.endsWith ^String % ".cljs")
+                 (map #(.getAbsolutePath ^java.io.File %)
+                      (mapcat #(file-seq (io/file (str dir java.io.File/separator %)))
+                              exclude-vec))))))
 
 (defn compile-root
   "Looks recursively in src-dir for .cljs files and compiles them, but
