5585bf36b6115ea29988d1340623de77c23cff70
David Nolen <david.nolen@gmail.com>
2013-05-20 08:21:40 -0400

    add INamed to the fast path list

--- src/clj/cljs/core.clj
+++ src/clj/cljs/core.clj
@@ -169,7 +169,7 @@
                  ISeqable ISequential IList IRecord IReversible ISorted IPrintWithWriter IWriter
                  IPrintWithWriter IPending IWatchable IEditableCollection ITransientCollection
                  ITransientAssociative ITransientMap ITransientVector ITransientSet
-                 IMultiFn IChunkedSeq IChunkedNext IComparable])
+                 IMultiFn IChunkedSeq IChunkedNext IComparable INamed])
           (iterate (fn [[p b]]
                      (if (core/== 2147483648 b)
                        [(core/inc p) 1]
