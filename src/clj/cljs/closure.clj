2b50a90f84700bbf9cc256ed9ab7f82f95d704da
Julien Eluard <julien.eluard@gmail.com>
2013-10-22 10:20:44 -0300

    CLJS-634: do not call getPath on a null value.

--- src/clj/cljs/closure.clj
+++ src/clj/cljs/closure.clj
@@ -734,8 +734,8 @@
                   (recur (next sources)
                     (if-let [url (:url source)]
                       (let [path (.getPath ^URL url)]
-                        (if-let [compiled (get @compiled-cljs path)] 
-                          (assoc merged (.getPath ^URL (:source-url source))
+                        (if-let [compiled (get @compiled-cljs path)]
+                          (assoc merged (when-let [source-url (:source-url source)] (.getPath ^URL source-url))
                             (sm/merge-source-maps
                               (:source-map compiled)
                               (get closure-source-map path)))
