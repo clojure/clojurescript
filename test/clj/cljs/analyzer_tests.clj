dba74da1db89eebf97cc4e1dedad2bf218132dc8
David Nolen <david.nolen@gmail.com>
2013-11-23 01:12:25 -0500

    add some basic inference tests

--- test/clj/cljs/analyzer_tests.clj
+++ test/clj/cljs/analyzer_tests.clj
@@ -1,5 +1,7 @@
 (ns cljs.analyzer-tests
-  (:require [cljs.analyzer :as a])
+  (:require [clojure.java.io :as io]
+            [cljs.analyzer :as a]
+            [cljs.env :as e])
   (:use clojure.test))
 
 ;;******************************************************************************
@@ -31,3 +33,60 @@
 
 (deftest all-warn
   (is (every? #(= 1 %) (map (fn [[name form]] (a/all-warn (warn-count form))) warning-forms))))
+
+;; =============================================================================
+;; Inference tests
+
+(def test-cenv (atom {}))
+(def test-env (assoc-in (a/empty-env) [:ns :name] 'cljs.core))
+
+(e/with-compiler-env test-cenv
+  (a/analyze-file (io/file "src/cljs/cljs/core.cljs")))
+
+(deftest basic-inference
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '1)))
+         'number))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '"foo")))
+         'string))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(make-array 10))))
+         'array))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(js-obj))))
+         'object))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '[])))
+         'cljs.core/IVector))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '{})))
+         'cljs.core/IMap))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '#{})))
+         'cljs.core/ISet))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env ())))
+         'cljs.core/IList))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(fn [x] x))))
+         'function)))
+
+(deftest if-inference
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(if true "foo" 1))))
+         '#{number string})))
+
+(deftest lib-inference
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(-conj [] 1))))
+         'clj))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(conj [] 1))))
+         'clj))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(assoc nil :foo :bar))))
+         'clj))
+  (is (= (e/with-compiler-env test-cenv
+           (:tag (a/analyze test-env '(dissoc {:foo :bar} :foo))))
+         'clj)))
