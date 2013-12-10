c3663240aeb1d9904c53df5cacbbbdd8ac374c51
David Nolen <david.nolen@gmail.com>
2013-12-10 11:57:30 -0500

    analyzer parse ns tests

--- test/clj/cljs/analyzer_tests.clj
+++ test/clj/cljs/analyzer_tests.clj
@@ -35,6 +35,79 @@
   (is (every? #(= 1 %) (map (fn [[name form]] (a/all-warn (warn-count form))) warning-forms))))
 
 ;; =============================================================================
+;; NS parsing
+
+(def ns-env (assoc-in (a/empty-env) [:ns [:name]] 'cljs.user))
+
+(deftest spec-validation
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require {:foo :bar})))
+          (catch Exception e
+            (.getMessage e)))
+        "Only [lib.ns & options] and lib.ns specs supported in :require / :require-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [:foo :bar])))
+          (catch Exception e
+            (.getMessage e)))
+        "Library name must be specified as a symbol in :require / :require-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [baz.woz :as woz :refer [] :plop])))
+          (catch Exception e
+            (.getMessage e)))
+        "Only :as alias and :refer (names) options supported in :require"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [baz.woz :as woz :refer [] :plop true])))
+          (catch Exception e
+            (.getMessage e)))
+        "Only :as and :refer options supported in :require / :require-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [baz.woz :as woz :refer [] :as boz :refer []])))
+          (catch Exception e
+            (.getMessage e)))
+        "Each of :as and :refer options may only be specified once in :require / :require-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:refer-clojure :refer [])))
+          (catch Exception e
+            (.getMessage e)))
+        "Only [:refer-clojure :exclude (names)] form supported"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:use [baz.woz :exclude []])))
+          (catch Exception e
+            (.getMessage e)))
+        "Only [lib.ns :only (names)] specs supported in :use / :use-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [baz.woz :as []])))
+          (catch Exception e
+            (.getMessage e)))
+        ":as must be followed by a symbol in :require / :require-macros"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require [baz.woz :as woz] [noz.goz :as woz])))
+          (catch Exception e
+            (.getMessage e)))
+        ":as alias must be unique"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:unless [])))
+          (catch Exception e
+            (.getMessage e)))
+        "Only :refer-clojure, :require, :require-macros, :use and :use-macros libspecs supported"))
+  (is (.startsWith
+        (try
+          (a/analyze ns-env '(ns foo.bar (:require baz.woz) (:require noz.goz)))
+          (catch Exception e
+            (.getMessage e)))
+        "Only one ")))
+
+;; =============================================================================
 ;; Inference tests
 
 (def test-cenv (atom {}))
