12aa3d3d8106120fc940e9ce6b8a899482bf3d9e
Brian Burns <bburns.km@gmail.com>
2014-04-07 08:44:19 -0500

    Add missing backslashes
    Note that \lib includes backslash, and it's more or less standard on Windows to not include backslash in environment variable paths. Otherwise get errors like so -
    
    > script\repl
    Clojure 1.5.1
    user=> (require '[cljs.repl :as repl])
    FileNotFoundException Could not locate cljs/repl__init.class or cljs/repl.clj on classpath:   clojure.lang.RT.load (RT.j
    ava:443)
    user=>

--- script/repl.bat
+++ script/repl.bat
@@ -3,7 +3,7 @@ setLocal EnableDelayedExpansion
 
 if "%CLOJURESCRIPT_HOME%" == "" set CLOJURESCRIPT_HOME=%~dp0..\
 
-set CLASSPATH=%CLOJURESCRIPT_HOME%src\clj;%CLOJURESCRIPT_HOME%src\cljs"
+set CLASSPATH=%CLOJURESCRIPT_HOME%\src\clj;%CLOJURESCRIPT_HOME%\src\cljs"
 for /R "%CLOJURESCRIPT_HOME%\lib" %%a in (*.jar) do (
   set CLASSPATH=!CLASSPATH!;%%a
 )
