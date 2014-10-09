3b98d20f0f89d49ce4edf4a8d845c7497bc051fc
dnolen <dnolen@cognitect.com>
2014-10-09 16:31:23 -0400

    fix iteration test, now eduction

--- test/cljs/cljs/core_test.cljs
+++ test/cljs/cljs/core_test.cljs
@@ -2369,7 +2369,7 @@
              (range 10)))
   (assert (= (sequence (mapcat reverse) [[3 2 1 0] [6 5 4] [9 8 7]])
              (range 10)))
-  (assert (= (seq (iteration (map inc) [1 2 3])) '(2 3 4)))
+  (assert (= (seq (eduction (map inc) [1 2 3])) '(2 3 4)))
   (assert (= (sequence (partition-by #{:split}) [1 2 3 :split 4 5 6])
              '([1 2 3] [:split] [4 5 6])))
   (assert (= (sequence (partition-all 3) '(1 2 3 4 5))
