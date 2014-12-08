2468794667a9be000bf2a3f1018dcc837cbc5e3b
David Nolen <david.nolen@gmail.com>
2014-12-08 06:55:18 -0500

    some missing notices

--- src/cljs/cljs/nodejscli.cljs
+++ src/cljs/cljs/nodejscli.cljs
@@ -1,3 +1,11 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 ; Projects compiled with :target :nodejs have this file appended.  Its
 ; job is to make sure cljs.nodejs is loaded and that the *main-cli-fn*
 ; is called with the script's command-line arguments.
