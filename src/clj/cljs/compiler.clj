clojure/clojurescript

commit 254e54876dfeae8c50d885010e730cdeaef26c99

forcing source maps to load for dynamic js reloads
This is the basic scetch

src/clj/cljs/compiler.clj
@@ -870,6 +870,7 @@
 (defn url-path [^File f]
   (.getPath (.toURL (.toURI f))))
 
+
 (defn compile-file*
   ([src dest] (compile-file* src dest nil))
   ([src dest opts]
@@ -912,7 +913,7 @@
                                 {:source-map (:source-map sm-data)}))]
                     (when (and sm-data (= (:optimizations opts) :none))
                       (let [sm-file (io/file (str (.getPath ^File dest) ".map"))]
-                        (emits "\n//# sourceMappingURL=" (.getName sm-file))
+                        (emits "\n//# sourceMappingURL=" (.getName sm-file) "?rel=" (System/currentTimeMillis))
                         (spit sm-file
                           (sm/encode {(url-path src) (:source-map sm-data)}
                             {:lines (+ (:gen-line sm-data) 2)

src/clj/cljs/source_map.clj
@@ -216,10 +216,12 @@
                    "file" (:file opts)
                    "sources" (into []
                                    (let [paths (keys m)
-                                         f (if (or (:output-dir opts)
+                                         f (comp
+                                            #(str % "?rel=" (System/currentTimeMillis))
+                                            (if (or (:output-dir opts)
                                                    (:source-map-path opts))
                                              #(relativize-path % opts)
-                                             #(last (string/split % #"/")))]
+                                             #(last (string/split % #"/"))))]
                                      (map f paths)))
                    "lineCount" (:lines opts)
                    "mappings" (->> (lines->segs (concat preamble-lines @lines))

commit 0e11e57a1f9973ca74bcb558e85fa6339e6a09d6

Fix Node.js source mapping regression introduced by commit 254e548
The browser-centric timestamp URL convention breaks
node-source-map-support.  The behavior now must be explicitly
requested by setting a new build flag `:source-map-timestamp` to
`true`.

src/clj/cljs/compiler.clj
@@ -913,7 +913,10 @@
                                 {:source-map (:source-map sm-data)}))]
                     (when (and sm-data (= (:optimizations opts) :none))
                       (let [sm-file (io/file (str (.getPath ^File dest) ".map"))]
-                        (emits "\n//# sourceMappingURL=" (.getName sm-file) "?rel=" (System/currentTimeMillis))
+                        (emits "\n//# sourceMappingURL=" (.getName sm-file)
+                          (if (true? (:source-map-timestamp opts))
+                            (str "?rel=" (System/currentTimeMillis))
+                            ""))
                         (spit sm-file
                           (sm/encode {(url-path src) (:source-map sm-data)}
                             {:lines (+ (:gen-line sm-data) 2)

src/clj/cljs/source_map.clj
@@ -217,11 +217,13 @@
                    "sources" (into []
                                    (let [paths (keys m)
                                          f (comp
-                                            #(str % "?rel=" (System/currentTimeMillis))
-                                            (if (or (:output-dir opts)
+                                             (if (true? (:source-map-timestamp opts))
+                                               #(str % "?rel=" (System/currentTimeMillis))
+                                               identity)
+                                             (if (or (:output-dir opts)
                                                    (:source-map-path opts))
-                                             #(relativize-path % opts)
-                                             #(last (string/split % #"/"))))]
+                                               #(relativize-path % opts)
+                                               #(last (string/split % #"/"))))]
                                      (map f paths)))
                    "lineCount" (:lines opts)
                    "mappings" (->> (lines->segs (concat preamble-lines @lines))