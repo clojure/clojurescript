4910bac070aaa8b5adf0cd21c887707ecf6cf539
Pieter van Prooijen <pieter.van.prooijen@xs4all.nl>
2015-02-02 14:04:12 +0100

    add beter usage examples

--- src/clj/cljs/repl/nashorn.clj
+++ src/clj/cljs/repl/nashorn.clj
@@ -18,36 +18,44 @@
 
 ;; Nashorn Clojurescript repl binding.
 ;;
-;; ** Usage from Leiningen:
-;;
-;; Create a file init_repl_test.clj containing (adjust :output-dir to your cljsbuild settings):
-;; (ns init-repl-test
-;;   (:require [cljs.repl]
-;;             [frontpage-client.nashorn-repl]))
-;;
-;; (def env (frontpage-client.nashorn-repl/repl-env))
-;; (cljs.repl/repl env :output-dir "resources/public/compiled")
-;;
-;; Invoke it with:
-;; lein trampoline run -m clojure.main src-clj/init_repl_test.clj
-;;
-;; ** Usage from nrepl / piggieback, execute the following at the nrepl prompt:
-;;    (adjust :output-dir to your cljsbuild settings)
-
-;; (ns init-repl-piggieback
-;;   (:require [frontpage-client.nashorn-repl]
-;;             [cemerick.piggieback]))
-;;
-;; (cemerick.piggieback/cljs-repl :repl-env (frontpage-client.nashorn-repl/repl-env)
-;;                                :output-dir "resources/public/compiled")
-;;
-;;
 ;; Uses the Nashorn load() function to load Javascript files into the script engine.
 ;;
 ;; Nashorn's load() function docs:
 ;; http://docs.oracle.com/javase/8/docs/technotes/guides/scripting/nashorn/shell.html
 ;;
-;; Some functions are borrowed from: https://github.com/bodil/cljs-nashorn and the node.js repl code
+;; Some functions are copied from: https://github.com/bodil/cljs-nashorn and the node.js repl code
+
+;;
+;; ** Usage from Leiningen:
+;;
+;; Create a file init_repl_test.clj containing (adjust :output-dir to your cljsbuild settings):
+
+(comment
+  (ns init-repl-test
+    (:require [cljs.repl]
+              [cljs.repl.nashorn]))
+
+  (cljs.repl/repl (cljs.repl.nashorn/repl-env) :output-dir "resources/public/compiled"
+                  :cache-analysis true))
+
+;;
+;; Invoke it with:
+;; lein trampoline run -m clojure.main src-clj/init_repl_test.clj
+
+;;
+;; ** Usage from nrepl / piggieback, execute the following at the nrepl prompt:
+;;    (adjust :output-dir to your cljsbuild settings)
+
+(comment
+  (ns init-repl-piggieback
+    (:require [cljs.repl.nashorn]
+              [cemerick.piggieback]))
+
+  (cemerick.piggieback/cljs-repl :repl-env (cljs.repl.nashorn/repl-env)
+                                 :output-dir "resources/public/compiled"
+                                 :cache-analysis true))
+
+;; Implementation
 
 (defn create-engine []
   (if-let [engine (.getEngineByName (ScriptEngineManager.) "nashorn")]
