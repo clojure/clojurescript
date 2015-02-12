0697a7495d55e61699bf0cf05befa48c56b14591
David Nolen <david.nolen@gmail.com>
2015-02-11 22:22:28 -0500

    require cljs.repl macros in cljs.repl

--- src/cljs/cljs/repl.cljs
+++ src/cljs/cljs/repl.cljs
@@ -6,7 +6,8 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(ns cljs.repl)
+(ns cljs.repl
+  (:require-macros cljs.repl))
 
 (defn print-doc [m]
   (println "-------------------------")
