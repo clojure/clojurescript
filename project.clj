31494aa6b1f09dc3e72e9aa09a904933c8c9acaf
David Nolen <david.nolen@gmail.com>
2015-03-03 12:08:37 -0500

    AOT everything when building uberjar

--- project.clj
+++ project.clj
@@ -15,7 +15,8 @@
                  [com.google.javascript/closure-compiler "v20150126"]
                  [org.mozilla/rhino "1.7R5"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :uberjar {:aot :all}}
   :aliases {"test-all" ["with-profile" "test,1.5:test,1.6" "test"]
             "check-all" ["with-profile" "1.5:1.6" "check"]}
   :min-lein-version "2.0.0")
