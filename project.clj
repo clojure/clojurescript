aced83b21118b6d64fba566b67e7a4f49a6cfbc8
David Nolen <david.nolen@gmail.com>
2015-03-04 07:48:56 -0500

    include src/aot_cljs to the classpath

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ^:replace ["-Xmx512m" "-server"]
   :source-paths ["src/clj"]
-  :resource-paths ["src/cljs"]
+  :resource-paths ["src/cljs" "src/aot_cljs"]
   :test-paths ["test/clj"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.3"]
