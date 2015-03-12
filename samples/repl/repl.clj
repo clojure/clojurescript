d07a3e9f3b76075be2b0cb0e213efcefe977d21b
David Nolen <david.nolen@gmail.com>
2015-03-12 19:03:37 -0400

    tweak samples/repl/repl.clj helper script

--- samples/repl/repl.clj
+++ samples/repl/repl.clj
@@ -2,8 +2,9 @@
 (require '[cljs.repl :as repl])
 (require '[cljs.repl.browser :as brepl])
 
-(repl/repl
-  (brepl/repl-env)
-  :init (fn []
-          (cljsc/build "src" {:output-to "main.js"}))
+(cljsc/build "src"
+  {:output-to "main.js"
+   :verbose true})
+
+(repl/repl (brepl/repl-env)
   :repl-verbose true)
