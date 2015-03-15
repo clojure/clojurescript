7e1f81a036b3621f847f2288e177f7d3242a0b04
David Nolen <david.nolen@gmail.com>
2015-03-15 12:27:19 -0400

    need to bind cljs.repl/*repl-opts* sooner for IJavaScriptEnv -setup so cljs form evaluations work

--- src/clj/cljs/repl.clj
+++ src/clj/cljs/repl.clj
@@ -692,7 +692,8 @@
                  :undeclared-var warn-on-undeclared
                  :undeclared-ns warn-on-undeclared
                  :undeclared-ns-form warn-on-undeclared)
-               ana/*cljs-static-fns* static-fns]
+               ana/*cljs-static-fns* static-fns
+               *repl-opts* opts]
        ;; TODO: the follow should become dead code when the REPL is
        ;; sufficiently enhanced to understand :cache-analysis - David
        (let [env {:context :expr :locals {}}
