46ea0b9c2871532334c1007546191e1b4e60dc9f
dnolen <dnolen@cognitect.com>
2015-05-07 17:12:32 -0400

    CLJS-1252: Update Closure Compiler Dependency to v20150505

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.reader "0.9.2"]
                  [org.clojure/google-closure-library "0.0-20140718-946a7d39"]
-                 [com.google.javascript/closure-compiler "v20150126"]
+                 [com.google.javascript/closure-compiler "v20150505"]
                  [org.mozilla/rhino "1.7R5"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :uberjar {:aot :all :main clojure.main}}
