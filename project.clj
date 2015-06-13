a039d0e63159a353df75bf3545ba2439d7af56eb
David Nolen <david.nolen@gmail.com>
2015-06-12 22:24:45 -0400

    Bump Closure Compiler to the latest

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.reader "0.9.2"]
                  [org.clojure/google-closure-library "0.0-20150505-021ed5b3"]
-                 [com.google.javascript/closure-compiler "v20150505"]
+                 [com.google.javascript/closure-compiler "v20150609"]
                  [org.mozilla/rhino "1.7R5"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :uberjar {:aot :all :main clojure.main}}
