b01c55c5e8e6e1bf52cb61e0fd0ed69e95707c1a
David Nolen <david.nolen@gmail.com>
2015-07-21 09:07:15 -0400

    missing doto

--- src/main/cljs/cljs/source_map.cljs
+++ src/main/cljs/cljs/source_map.cljs
@@ -249,7 +249,7 @@
                                      (map (set/map-invert @names->idx)
                                        (range (count @names->idx))))}
             (:sources-content opts)
-            (gobj/set "sourcesContent" (:sources-content opts)))]
+            (doto (gobj/set "sourcesContent" (:sources-content opts))))]
       (.stringify js/JSON source-map-file-contents))))
 
 ;; -----------------------------------------------------------------------------
