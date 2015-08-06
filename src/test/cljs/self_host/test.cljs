836609cf5228b0ebb9d5342828f57cac599a92ee
dnolen <dnolen@cognitect.com>
2015-08-05 20:39:07 -0400

    more compile-str tests

--- src/test/cljs/self_host/test.cljs
+++ src/test/cljs/self_host/test.cljs
@@ -17,9 +17,21 @@
 
 (deftest test-compile-str
   (async done
-    (let [l (latch 1 done)]
+    (let [l (latch 3 done)]
       (cljs/compile-str st "(+ 1 1)"
         (fn [{:keys [error value]}]
           (is (nil? error))
           (is (= value "(1 + 1);\n"))
+          (inc! l)))
+      (cljs/compile-str st "(fn [])" nil
+        {:context :expr}
+        (fn [{:keys [error value]}]
+          (is (nil? error))
+          (is (= value "(function (){\nreturn null;\n})"))
+          (inc! l)))
+      (cljs/compile-str st "(if cljs.core.first 1 2)" nil
+        {:context :expr}
+        (fn [{:keys [error value]}]
+          (is (nil? error))
+          (is (= value "(cljs.core.truth_(cljs.core.first)?1:2)"))
           (inc! l))))))
