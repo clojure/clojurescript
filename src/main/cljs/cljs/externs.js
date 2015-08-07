42dc1f58bc71a0059490064ff85096954546e7e1
dnolen <dnolen@cognitect.com>
2015-08-07 16:15:51 -0400

    remove other es6 externs now included

--- src/main/cljs/cljs/externs.js
+++ src/main/cljs/cljs/externs.js
@@ -1,26 +1,5 @@
 Math.imul = function(a, b) {};
 
-var cljs = {};
-cljs.core = {};
-/**
- * @constructor;
- */
-cljs.core.Iterator = function() {};
-cljs.core.Iterator.prototype.next = function() {};
-
-/**
- * @constructor;
- */
-function IteratorStep() {};
-/**
- * @type {boolean}
- */
-IteratorStep.prototype.done;
-/**
- * @type {Object}
- */
-IteratorStep.prototype.value;
-
 /**
  * @constructor;
  */
