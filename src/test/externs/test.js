a5c164c9dc4404374a790a0f6796b72c07b4fe91
David Nolen <david.nolen@gmail.com>
2017-01-23 08:29:54 -0500

    add more extern cases

--- src/test/externs/test.js
+++ src/test/externs/test.js
@@ -4,8 +4,20 @@
 var Foo = function() {};
 Foo.prototype.wozMethod = function() {
 };
-
 /**
  * @return {Foo}
  */
 var baz = function() {};
+/**
+ * @constructor
+ */
+Foo.Bar = function() {};
+/**
+ * @return {Foo.Boo}
+ */
+Foo.Bar.prototype.baz = function() {};
+/**
+ * @constructor
+ */
+Foo.Boo = function() {};
+Foo.Boo.prototype.woz = function() {};
