19cc3945d302d9e3c2da103c9a5d42ff31ad8366
dnolen <dnolen@cognitect.com>
2017-01-23 16:20:08 -0500

    update example, works fine

--- src/test/clojure/cljs/analyzer_tests.clj
+++ src/test/clojure/cljs/analyzer_tests.clj
@@ -846,7 +846,6 @@
           (map (comp :externs second)
             (get @test-cenv ::a/namespaces))))))
 
-  ;; FIXME: we don't get an extern for Component
   (let [test-cenv (atom {::a/externs (externs/externs-map
                                        (closure/load-externs
                                          {:externs ["src/test/externs/test.js"]}))})]
