73cd8aa228721780f1957dea842427c3f1bef1b6
Mike Fikes <mike@fikesfarm.com>
2017-01-27 02:03:37 -0500

    CLJS-1911: Need to bind Node.js require

--- src/main/cljs/cljs/bootstrap_node.js
+++ src/main/cljs/cljs/bootstrap_node.js
@@ -82,6 +82,7 @@ function nodeGlobalRequire(file) {
   var _module = global.module, _exports = global.exports;
   global.module = undefined;
   global.exports = undefined;
+  global.require = require;
   vm.runInThisContext(fs.readFileSync(file), file);
   global.exports = _exports;
   global.module = _module;
