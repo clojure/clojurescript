296d0a69340e832b92ed742b3cd0304a06bea27f
António Nuno Monteiro <anmonteiro@gmail.com>
2017-03-27 11:44:28 -0700

    CLJS-1988: add :npm-deps to recognized compiler options

--- src/main/clojure/cljs/closure.clj
+++ src/main/clojure/cljs/closure.clj
@@ -166,7 +166,7 @@
     :pretty-print :print-input-delimiter :pseudo-names :recompile-dependents :source-map
     :source-map-inline :source-map-timestamp :static-fns :target :verbose :warnings
     :emit-constants :ups-externs :ups-foreign-libs :ups-libs :warning-handlers :preloads
-    :browser-repl :cache-analysis-format :infer-externs :closure-generate-exports})
+    :browser-repl :cache-analysis-format :infer-externs :closure-generate-exports :npm-deps})
 
 (def string->charset
   {"iso-8859-1" StandardCharsets/ISO_8859_1
