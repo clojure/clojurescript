ec670068e7e3f06040b614e0949ff74ed5c669b4
António Nuno Monteiro <anmonteiro@gmail.com>
2017-07-09 11:26:11 -0700

    CLJS-2201: Self-host: test-js-filename failing

--- src/test/cljs/cljs/test_test.cljs
+++ src/test/cljs/cljs/test_test.cljs
@@ -29,4 +29,4 @@
     (is (nan? column))))
 
 (deftest test-js-filename
-  (is (= "core-advanced-test.js" (ct/js-filename "nW@builds/out-adv/core-advanced-test.js:1191:77"))))
+  (is (= "core-advanced-test.js" (ct/js-filename (str "nW@" (ct/cljs-output-dir) "/core-advanced-test.js:1191:77")))))
