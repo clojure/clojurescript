fd1784b4b167dff3d8f16c98d7e53c59409ee9d0
Mike Fikes <mike@fikesfarm.com>
2018-04-07 10:47:40 -0400

    CLJS-2713: test-reader fails on Windows

--- script/test.ps1
+++ script/test.ps1
@@ -11,7 +11,7 @@ $targets =
     @{ env="CHAKRACORE_HOME"; name="ChakraCore"; cmd={ & "$env:CHAKRACORE_HOME\ch" $testjs } }
 $ran = 0
 
-$opts = '{:optimizations :advanced :output-wrapper true :verbose true :compiler-stats true :parallel-build true :output-dir \"builds/out-adv\" :npm-deps {:lodash \"4.17.4\"} :closure-warnings {:non-standard-jsdoc :off :global-this :off} :install-deps true :language-in :es6 :language-out :es5 :foreign-libs [{:file \"src/test/cljs/calculator_global.js\" :provides [\"calculator\"] :global-exports {calculator Calculator}} {:file \"src/test/cljs/es6_dep.js\" :module-type :es6 :provides [\"es6_calc\"]} {:file \"src/test/cljs/calculator.js\" :module-type :commonjs :provides [\"calculator\"]} {:file \"src/test/cljs/es6_default_hello.js\" :provides [\"es6_default_hello\"] :module-type :es6}]}"'
+$opts = $('{:optimizations :advanced :output-wrapper true :verbose true :compiler-stats true :parallel-build true :output-dir \"builds/out-adv\" :output-to \"' + $testjs + '\" :npm-deps {:lodash \"4.17.4\"} :closure-warnings {:non-standard-jsdoc :off :global-this :off} :install-deps true :language-in :es6 :language-out :es5 :foreign-libs [{:file \"src/test/cljs/calculator_global.js\" :provides [\"calculator\"] :global-exports {calculator Calculator}} {:file \"src/test/cljs/es6_dep.js\" :module-type :es6 :provides [\"es6_calc\"]} {:file \"src/test/cljs/calculator.js\" :module-type :commonjs :provides [\"calculator\"]} {:file \"src/test/cljs/es6_default_hello.js\" :provides [\"es6_default_hello\"] :module-type :es6}]}"')
 
 function Test-It($env, $name, [scriptblock] $cmd) {
     $env_val = if(Test-Path env:$env) { (Get-Item env:$env).Value } else { "" }
@@ -32,7 +32,7 @@ try {
 
     New-Item builds\out-adv -ItemType Directory -Force | Out-Null
 
-    bin\cljsc src\test\cljs $opts | Set-Content $testjs
+    bin\cljsc src\test\cljs $opts
 
     $targets | Foreach-Object { Test-It @_ }
 }
