9e1ff2232da00dcda7fb495784e7d5a1d72573a7
Mike Fikes <mike@fikesfarm.com>
2018-09-01 08:27:12 -0400

    CLJS-2878: Update Closure Compiler to v20180805

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [org.clojure/test.check "0.10.0-alpha3" :scope "test"]
                  [com.cognitect/transit-clj "0.8.309"]
                  [org.clojure/google-closure-library "0.0-20170809-b9c14c6b"]
-                 [com.google.javascript/closure-compiler-unshaded "v20180716"]
+                 [com.google.javascript/closure-compiler-unshaded "v20180805"]
                  [org.mozilla/rhino "1.7R5"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :uberjar {:aot :all :main cljs.main}
