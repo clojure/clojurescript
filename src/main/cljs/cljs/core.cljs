f2aaa2a2211fb734aa1083a31160aabd51d1e4d7
David Nolen <david.nolen@gmail.com>
2019-01-01 12:25:23 -0500

    CLJS-3028: atom docstring typo
    "be come" should be "become", same as CLJ-1055

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -4441,7 +4441,7 @@ reduces them without incurring seq initialization"
 
   :validator validate-fn
 
-  If metadata-map is supplied, it will be come the metadata on the
+  If metadata-map is supplied, it will become the metadata on the
   atom. validate-fn must be nil or a side-effect-free fn of one
   argument, which will be passed the intended new state on any state
   change. If the new state is unacceptable, the validate-fn should
