df1837048d01b157a04bb3dc7fedc58ee349a24a
Mike Fikes <mike@fikesfarm.com>
2019-06-15 23:16:56 -0400

    CLJS-3107: Eliminate checked ifs in TransientArrayMap

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -7025,7 +7025,7 @@ reduces them without incurring seq initialization"
 
 (declare array->transient-hash-map)
 
-(deftype TransientArrayMap [^:mutable editable?
+(deftype TransientArrayMap [^:mutable ^boolean editable?
                             ^:mutable len
                             arr]
   ICounted
