7d4325232ae180d367e6764ccf3ea58986f6ef8a
roman01la <roman01la@romanliutikov.com>
2019-11-18 13:59:27 +0100

    CLJS-3193: Optimize cljs.core/re-pattern

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -10022,7 +10022,9 @@ reduces them without incurring seq initialization"
   (if (instance? js/RegExp s)
     s
     (let [[prefix flags] (re-find #"^\(\?([idmsux]*)\)" s)
-          pattern (subs s (count prefix))]
+          pattern (subs s (if (nil? prefix)
+                            0
+                            (count ^string prefix)))]
       (js/RegExp. pattern (or flags "")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Printing ;;;;;;;;;;;;;;;;
