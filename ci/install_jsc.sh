3fdaabedb1343f434b6cb4f75fa28e748f96eff0
David Nolen <david.nolen@gmail.com>
2021-03-16 11:10:52 -0400

    switch to OS X runner, use stable WebKit tag, drop lein - use depstar for uberjar step

--- ci/install_jsc.sh
+++ ci/install_jsc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ ! -d WebKit ]
 then
-  git clone -b Safari-611.1.5.1 --depth=1 git@github.com:WebKit/WebKit.git WebKit;
+  git clone -b Safari-611.1.5.1 --depth=1 https://github.com/WebKit/WebKit.git WebKit;
   cd WebKit;
   Tools/Scripts/build-jsc --jsc-only;
   cd ..
