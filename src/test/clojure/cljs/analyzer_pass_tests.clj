4ba58e66b138abcb604803c6df2ab17f044cf443
David Nolen <david.nolen@gmail.com>
2021-05-12 09:54:02 -0400

    (wip) reorg

--- src/test/clojure/cljs/analyzer_pass_tests.clj
+++ src/test/clojure/cljs/analyzer_pass_tests.clj
@@ -8,133 +8,49 @@
 
 (ns cljs.analyzer-pass-tests
   (:require [cljs.analyzer :as ana]
+            [cljs.analyzer.passes.and-or :as and-or]
             [cljs.analyzer-tests :as ana-tests :refer [analyze]]
             [cljs.compiler-tests :as comp-tests :refer [emit]]
             [clojure.test :as test :refer [deftest is testing]]))
 
-(def simple-ops
-  #{:var :js-var :local :invoke :const :host-field :host-call :js :quote})
-
-(defn ->expr-env [ast]
-  (assoc-in ast [:env :context] :expr))
-
-(defn simple-op? [ast]
-  (contains? simple-ops (:op ast)))
-
-(defn simple-test-expr?
-  [ast]
-  (boolean
-    (and (simple-op? ast)
-         ('#{boolean seq} (:tag ast)))))
-
-(defn single-binding-let? [ast]
-  (and (= :let (:op ast))
-       (= 1 (count (-> ast :bindings)))))
-
-(defn no-statements? [let-ast]
-  (= [] (-> let-ast :body :statements)))
-
-(defn returns-if? [let-ast]
-  (= :if (-> let-ast :body :ret :op)))
-
-(defn simple-test-binding-let? [ast]
-  (and (single-binding-let? ast)
-       (no-statements? ast)
-       (simple-test-expr? (-> ast :bindings first :init))
-       (returns-if? ast)))
-
-(defn test=then? [if-ast]
-  ;; remove :env, if same, local will differ only by
-  ;; :context (:expr | :statement)
-  (= (dissoc (:test if-ast) :env)
-     (dissoc (:then if-ast) :env)))
-
-(defn test=else? [if-ast]
-  ;; remove :env, if same, local will differ only by
-  ;; :context (:expr | :statement)
-  (= (dissoc (:test if-ast) :env)
-     (dissoc (:else if-ast) :env)))
-
-(defn simple-and? [ast]
-  (and (simple-test-binding-let? ast)
-       (test=else? (-> ast :body :ret))))
-
-(defn simple-or? [ast]
-  (and (simple-test-binding-let? ast)
-       (test=then? (-> ast :body :ret))))
-
-(defn optimizable-and? [ast]
-  (and (simple-and? ast)
-       (simple-test-expr? (-> ast :body :ret :then))))
-
-(defn optimizable-or? [ast]
-  (and (simple-or? ast)
-       (simple-test-expr? (-> ast :body :ret :else))))
-
-(defn optimize-and [ast]
-  {:op :js
-   :env (:env ast)
-   :segs ["(" ") && (" ")"]
-   :args [(-> ast :bindings first :init)
-          (->expr-env (-> ast :body :ret :then))]
-   :form (:form ast)
-   :children [:args]
-   :tag 'boolean})
-
-(defn optimize-or [ast]
-  {:op :js
-   :env (:env ast)
-   :segs ["(" ") || (" ")"]
-   :args [(-> ast :bindings first :init)
-          (->expr-env (-> ast :body :ret :else))]
-   :form (:form ast)
-   :children [:args]
-   :tag 'boolean})
-
-(defn optimize-and-or [env ast _]
-  (cond
-    (optimizable-and? ast) (optimize-and ast)
-    (optimizable-or? ast)  (optimize-or ast)
-    :else ast))
-
 (deftest test-helpers
   (testing "Testing and/or matching helpers"
     (let [ast (analyze (ana/empty-env) `(and true false))]
-      (is (simple-op? (-> ast :bindings first :init)))
-      (is (simple-test-expr? (-> ast :bindings first :init)))
-      (is (single-binding-let? ast))
-      (is (simple-test-binding-let? ast))
-      (is (no-statements? ast))
-      (is (returns-if? ast)))))
+      (is (and-or/simple-op? (-> ast :bindings first :init)))
+      (is (and-or/simple-test-expr? (-> ast :bindings first :init)))
+      (is (and-or/single-binding-let? ast))
+      (is (and-or/simple-test-binding-let? ast))
+      (is (and-or/no-statements? ast))
+      (is (and-or/returns-if? ast)))))
 
 (deftest and-or-matchers
   (testing "Testing and/or ast matching"
     (let [ast (analyze (ana/empty-env) `(and true false))]
-      (is (simple-and? ast))
-      (is (optimizable-and? ast)))
+      (is (and-or/simple-and? ast))
+      (is (and-or/optimizable-and? ast)))
     (let [ast (analyze (ana/empty-env) `(or true false))]
-      (is (simple-or? ast))
-      (is (optimizable-or? ast)))))
+      (is (and-or/simple-or? ast))
+      (is (and-or/optimizable-or? ast)))))
 
 (deftest and-or-code-gen
   (testing "and/or optimization code gen"
     (let [expr-env (assoc (ana/empty-env) :context :expr)
           ast      (->> `(and true false)
                      (analyze expr-env)
-                     optimize-and)
+                     and-or/optimize-and)
           code     (with-out-str (emit ast))]
       (is (= code "(true) && (false)")))
     (let [expr-env (assoc (ana/empty-env) :context :expr)
           ast      (->> `(or true false)
                      (analyze expr-env)
-                     optimize-or)
+                     and-or/optimize-or)
           code     (with-out-str (emit ast))]
       (is (= code "(true) || (false)")))))
 
 (deftest and-or-code-gen-pass
   (testing "and/or optimization code gen pass"
     (let [expr-env (assoc (ana/empty-env) :context :expr)
-          ast      (binding [ana/*passes* (into [optimize-and-or] ana/*passes*)]
+          ast      (binding [ana/*passes* (into [and-or/optimize] ana/*passes*)]
                      (->> `(and true false)
                        (analyze expr-env)))
           code     (with-out-str (emit ast))]
