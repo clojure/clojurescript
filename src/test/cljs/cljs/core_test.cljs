765acba4aadd03d487fc21aabb584c4a42e9b68d
David Nolen <david.nolen@gmail.com>
2024-02-27 12:44:21 -0500

    * self-parity fails if add test to core so move test to own ns

--- src/test/cljs/cljs/core_test.cljs
+++ src/test/cljs/cljs/core_test.cljs
@@ -2042,15 +2042,3 @@
       (is (= 11 @c))
       (is (= [11 0] (reset-vals! c 0)))
       (is (= 0 @c)))))
-
-(defn cljs-3411-function
-  "this function adds two numbers"
-  {:test #(do
-            (assert (= (cljs-3411-function 2 3) 5))
-            (assert (= (cljs-3411-function 4 4) 8)))}
-  ([x y] (+ x y)))
-
-(deftest cljs-3411
-  (testing "cljs.core/test respects docstring"
-    (is (= :ok (test cljs-3411)))
-    (is (= :ok (test #'cljs-3411)))))
