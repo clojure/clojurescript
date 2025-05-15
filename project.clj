92040f79d17f3b26870fbf5b7231b8f4a74440da
David Nolen <david.nolen@gmail.com>
2025-05-15 17:03:58 -0400

    - delete pom.xml - remove tools.reader as dep, vendorized - add latest GCL release

--- project.clj
+++ project.clj
@@ -11,10 +11,9 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/core.specs.alpha "0.1.24"]
-                 [org.clojure/tools.reader "1.3.6"]
                  [org.clojure/test.check "1.1.1" :scope "test"]
                  [com.cognitect/transit-java "1.0.362"]
-                 [org.clojure/google-closure-library "0.0-20250418-2ce9ab6d"]
+                 [org.clojure/google-closure-library "0.0-20250515-f04e4c0e"]
                  [com.google.javascript/closure-compiler-unshaded "v20250402"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :uberjar {:aot :all :main cljs.main}
