3852e6749864816880900fcf0482396a421349eb
davidnolen <david.nolen@gmail.com>
2025-07-05 13:24:55 -0400

    * add compiler test case for inferring return of Number.isNaN

--- src/test/clojure/cljs/compiler_tests.clj
+++ src/test/clojure/cljs/compiler_tests.clj
@@ -15,7 +15,8 @@
             [cljs.util :as util]
             [cljs.tagged-literals :as tags]
             [clojure.java.io :as io]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.test :as test])
   (:import [java.io File]))
 
 (defn analyze
@@ -373,6 +374,13 @@
                        (let [window js/window]
                          window))]))]
       (is (re-find #"window__\$1" code)))))
+
+(deftest test-externs-infer-is-nan
+  (testing "Let binding which use JS global names should get shadowed"
+    (let [code (env/with-compiler-env (env/default-compiler-env)
+                 (compile-form-seq
+                   '[(if (.isNaN js/Number 1) true false)]))]
+      (is (nil? (re-find #"truth_" code))))))
 
 ;; CLJS-1225
 
