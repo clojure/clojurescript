66276a42eebb2f05fffd508c0665a698550d46d0
davidnolen <david.nolen@gmail.com>
2025-10-03 20:50:41 -0400

    remove typo

--- src/test/cljs_build/trivial/core6.cljs
+++ src/test/cljs_build/trivial/core6.cljs
@@ -1,3 +1,3 @@
 (ns trivial.core6)
 
-(.log js/console (->> (map inc (range 10)) (filter even?) (partition 2) (drop 1) (mapcat identity) into-array)})
+(.log js/console (->> (map inc (range 10)) (filter even?) (partition 2) (drop 1) (mapcat identity) into-array))
